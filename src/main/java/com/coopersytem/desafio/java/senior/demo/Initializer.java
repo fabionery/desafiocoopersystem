dda909710a95410754fce8b82446ecf826f7ef26
fabio.nery <fabio.nery@fornecedores.sicoob.com.br>
2019-05-21 19:00:30 -0300

    desafio

---
+++ src/main/java/com/coopersytem/desafio/java/senior/demo/Initializer.java
@@ -0,0 +1,40 @@
+package com.coopersytem.desafio.java.senior.demo;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.coopersytem.desafio.java.senior.demo.model.Event;
+import com.coopersytem.desafio.java.senior.demo.model.Group;
+import com.coopersytem.desafio.java.senior.demo.model.GroupRepository;
+
+@Component
+class Initializer implements CommandLineRunner {
+
+    private final GroupRepository repository;
+
+    public Initializer(GroupRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) {
+        Stream.of("Denver JUG", "Utah JUG", "Seattle JUG",
+                "Richmond JUG").forEach(name ->
+                repository.save(new Group(name))
+        );
+
+        Group djug = repository.findByName("Denver JUG");
+        Event e = Event.builder().title("Full Stack Reactive")
+                .description("Reactive with Spring Boot + React")
+                .date(Instant.parse("2018-12-12T18:00:00.000Z"))
+                .build();
+        djug.setEvents(Collections.singleton(e));
+        repository.save(djug);
+
+        repository.findAll().forEach(System.out::println);
+    }
+}
